55862dcb2f388a3aba65e43f5b2504ef2a4a81bb
Prakhar <rinayush@gmail.com>
2022-03-08 15:25:57 +0530

    14 done

---
+++ 14.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    ifstream f1;
+    f1.open("14input.txt");
+
+    ofstream f2;
+    f2.open("14output.txt");
+
+    while (f1.eof()==0)
+    {
+        getline(f1, str);
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str[i] == ' ')
+            {
+                continue;
+            }
+
+            f2 << str[i];
+        }
+
+        f2 << endl;
+        str.clear();
+    }
+    return 0;
+}
